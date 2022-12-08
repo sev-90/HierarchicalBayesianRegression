61bde29c5dc66e565802f8df9198a15feed77e7b
Sevin <sm4894@columbia.edu>
2022-12-08 14:04:56 -0500

    initial commit

---
+++ model_evaluation.py
@@ -0,0 +1,271 @@
+import numpy as np
+import pickle
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+# import arviz as az
+
+class models_comparison(object):
+    def __init__(self, data_type = None ):
+        self.data_type = data_type
+        self.load_data()
+        # self.udtt_plot_predictions()
+        # self.tt_predictions_plot()
+        self.print_error()
+        # self.plot_predictions_byRegions()
+        # self.load_models()
+        # self.test_model()
+    def load_data(self):
+        data_path = '/home/sevin/Desktop/projects/TravelTime_Prediction/HBRegression/TT_pred_wHBR/data/modeling_data/{}/'.format(self.data_type)
+        self.train_df = pd.read_csv(data_path + 'train_wpreds.txt')
+        # print(self.train_df['Orgn_Drgn'].unique())
+        # exit()
+        self.test_df = pd.read_csv(data_path + 'test_wpreds.txt')
+        with open(data_path + 'scale_parameters.pickle', 'rb') as handle:
+            scale_parameters = pickle.load(handle)
+        
+        self.variables_means = scale_parameters['means']
+        self.variables_stds = scale_parameters['stds']
+        # print(self.test_df)
+        # exit()
+
+
+    def udtt_plot_predictions(self):
+        nplot = 100
+        fig, ax = plt.subplots(ncols = 2, figsize=(10,5))
+        # ax[0].plot(self.train_df.unitDist_TT_hpm[:], self.train_df.hbr_predMean_onTrain[:],'b.' , markersize=0.5)
+        # ax[0].plot(self.train_df.unitDist_TT_hpm[:], self.train_df.ngboost_predMean_onTrain[:],'r.' ,markersize=0.5)
+        ax[0].errorbar(x = self.train_df.unitDist_TT_hpm[:], y = self.train_df.hbr_udtt_predMean[:], yerr= self.train_df.hbr_udtt_predStd[:], fmt='b.',markersize=0.5, alpha=0.2)
+        ax[0].errorbar(x =self.train_df.unitDist_TT_hpm[:], y = self.train_df.ngboost_udtt_predMean[:], yerr= self.train_df.ngboost_udtt_predStd[:], fmt='r.', markersize=0.5,alpha=0.2)
+        ax[0].plot([0,0.25],[0,0.25])
+        ax[0].set_xlabel('True')
+        ax[0].set_ylabel('Pred')
+        # ax[1].plot(self.test_df.unitDist_TT_hpm[:], self.test_df.hbr_predMean_onTest[:],'b.',markersize=0.5 )
+        # ax[1].plot(self.test_df.unitDist_TT_hpm[:], self.test_df.ngboost_predMean_onTest[:],'r.',markersize=0.5 )
+        ax[1].errorbar(x = self.test_df.unitDist_TT_hpm[:], y = self.test_df.hbr_udtt_predMean[:], yerr= self.test_df.hbr_udtt_predStd[:], fmt='b.',markersize=0.5, alpha=0.2)
+        ax[1].errorbar(x =self.test_df.unitDist_TT_hpm[:], y = self.test_df.ngboost_udtt_predMean[:], yerr= self.test_df.ngboost_udtt_predStd[:], fmt='r.', markersize=0.5,alpha=0.2)
+        ax[1].plot([0,0.25],[0,0.25])
+        ax[1].set_xlabel('True')
+        ax[1].set_ylabel('Pred')
+        plt.show()
+    
+    
+    def tt_predictions_plot(self):
+
+        nplot = 100
+        fig, ax = plt.subplots(ncols = 2, figsize=(10,5))
+        # ax[0].plot(self.train_df.unitDist_TT_hpm[:], self.train_df.hbr_predMean_onTrain[:],'b.' , markersize=0.5)
+        # ax[0].plot(self.train_df.unitDist_TT_hpm[:], self.train_df.ngboost_predMean_onTrain[:],'r.' ,markersize=0.5)
+        ax[0].errorbar(x = self.train_df.travel_time[:]/60., y = self.train_df.hbr_tt_predMean[:], yerr= self.train_df.hbr_tt_predStd[:], fmt='b.',markersize=0.5, alpha=0.2)
+        ax[0].errorbar(x =self.train_df.travel_time[:]/60., y = self.train_df.ngboost_tt_predMean[:], yerr= self.train_df.ngboost_tt_predStd[:], fmt='r.', markersize=0.5,alpha=0.2)
+        ax[0].plot([0,50],[0,50], color='k')
+        ax[0].set_xlabel('True')
+        ax[0].set_ylabel('Pred')
+        # ax[1].plot(self.test_df.unitDist_TT_hpm[:], self.test_df.hbr_predMean_onTest[:],'b.',markersize=0.5 )
+        # ax[1].plot(self.test_df.unitDist_TT_hpm[:], self.test_df.ngboost_predMean_onTest[:],'r.',markersize=0.5 )
+        ax[1].errorbar(x = self.test_df.travel_time[:]/60., y = self.test_df.hbr_tt_predMean[:], yerr= self.test_df.hbr_tt_predStd[:], fmt='b.',markersize=0.5, alpha=0.2)
+        ax[1].errorbar(x =self.test_df.travel_time[:]/60., y = self.test_df.ngboost_tt_predMean[:], yerr= self.test_df.ngboost_tt_predStd[:], fmt='r.', markersize=0.5,alpha=0.2)
+        ax[1].plot([0,50],[0,50], color='k')
+        ax[1].set_xlabel('True')
+        ax[1].set_ylabel('Pred')
+        plt.show()
+    
+    def mean_relative_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MRE = np.mean(np.abs((y_true - y_pred) / y_true)) 
+        print('mean relative error is {:0.4f}'.format(MRE) )
+        return MRE
+    
+    def mean_absolute_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MAE = np.mean(np.abs(y_true - y_pred)) * 60
+        print('mean absolute error is {:0.2f} '.format( MAE) )
+        return MAE
+    
+    def root_mean_sqr_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        RMSE = np.sqrt(mean_squared_error(y_true, y_pred)) * 60
+        print('root mean square error is {:0.2f} '.format(RMSE) )
+        return RMSE
+    
+    def median_absolute_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MedAE = np.median(np.abs(y_true - y_pred)) *60
+        print('median absolute error is {:0.2f}'.format(MedAE) )
+        return MedAE
+    
+    def median_relative_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MedRE = np.median(np.abs((y_true - y_pred) / y_true)) 
+        print('median relative error is {:0.4f}'.format(MedRE) )
+        return MedRE
+
+
+    def print_error(self):
+        # print('**** for hbr model the errors on tt are: ')
+        print("***mean relative errors:")
+        print("train")
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logn)
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logdirect)
+        # self.mean_relative_error( self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean)
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean_direct)
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.LRegression_tt_predMean_direct)
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.RForest_tt_predMean_direct)
+        self.mean_relative_error( self.train_df.travel_time/60., self.train_df.LGBM_tt_predMean_direct)
+        print("test")
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logn)
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logdirect)
+        # self.mean_relative_error( self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean)
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean_direct)
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.LRegression_tt_predMean_direct)
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.RForest_tt_predMean_direct)
+        self.mean_relative_error( self.test_df.travel_time/60., self.test_df.LGBM_tt_predMean_direct)
+        
+
+        print("***mean absolute errors:")
+        print("train")
+        self.mean_absolute_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logn)
+        self.mean_absolute_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logdirect)
+        # self.mean_absolute_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean)
+        self.mean_absolute_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean_direct)
+        self.mean_absolute_error( self.train_df.travel_time/60., self.train_df.LRegression_tt_predMean_direct)
+        self.mean_absolute_error( self.train_df.travel_time/60., self.train_df.RForest_tt_predMean_direct)
+        self.mean_absolute_error( self.train_df.travel_time/60., self.train_df.LGBM_tt_predMean_direct)
+        print("test")
+        self.mean_absolute_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logn)
+        self.mean_absolute_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logdirect)
+        # self.mean_absolute_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean)
+        self.mean_absolute_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean_direct)
+        self.mean_absolute_error( self.test_df.travel_time/60., self.test_df.LRegression_tt_predMean_direct)
+        self.mean_absolute_error( self.test_df.travel_time/60., self.test_df.RForest_tt_predMean_direct)
+        self.mean_absolute_error( self.test_df.travel_time/60., self.test_df.LGBM_tt_predMean_direct)
+        
+        print("***median relative errors:")
+        print("train")
+        self.median_relative_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logn)
+        self.median_relative_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logdirect)
+        # self.median_relative_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean)
+        self.median_relative_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean_direct)
+        self.median_relative_error( self.train_df.travel_time/60., self.train_df.LRegression_tt_predMean_direct)
+        self.median_relative_error( self.train_df.travel_time/60., self.train_df.RForest_tt_predMean_direct)
+        self.median_relative_error( self.train_df.travel_time/60., self.train_df.LGBM_tt_predMean_direct)
+        print("test")
+        self.median_relative_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logn)
+        self.median_relative_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logdirect)
+        # self.median_relative_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean)
+        self.median_relative_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean_direct)
+        self.median_relative_error( self.test_df.travel_time/60., self.test_df.LRegression_tt_predMean_direct)
+        self.median_relative_error( self.test_df.travel_time/60., self.test_df.RForest_tt_predMean_direct)
+        self.median_relative_error( self.test_df.travel_time/60., self.test_df.LGBM_tt_predMean_direct)
+
+        print("***median absolute errors:")
+        print("train")
+        self.median_absolute_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logn)
+        self.median_absolute_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logdirect)
+        # self.median_absolute_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean)
+        self.median_absolute_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean_direct)
+        self.median_absolute_error( self.train_df.travel_time/60., self.train_df.LRegression_tt_predMean_direct)
+        self.median_absolute_error( self.train_df.travel_time/60., self.train_df.RForest_tt_predMean_direct)
+        self.median_absolute_error( self.train_df.travel_time/60., self.train_df.LGBM_tt_predMean_direct)
+        print("test")
+        self.median_absolute_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logn)
+        self.median_absolute_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logdirect)
+        # self.median_absolute_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean)
+        self.median_absolute_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean_direct)
+        self.median_absolute_error( self.test_df.travel_time/60., self.test_df.LRegression_tt_predMean_direct)
+        self.median_absolute_error( self.test_df.travel_time/60., self.test_df.RForest_tt_predMean_direct)
+        self.median_absolute_error( self.test_df.travel_time/60., self.test_df.LGBM_tt_predMean_direct)
+
+        print("***root mean squared errors:")
+        print("train")
+        self.root_mean_sqr_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logn)
+        self.root_mean_sqr_error(self.train_df.travel_time/60., self.train_df.hbr_tt_predMean_logdirect)
+        # self.root_mean_sqr_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean)
+        self.root_mean_sqr_error(self.train_df.travel_time/60., self.train_df.ngboost_tt_predMean_direct)
+        self.root_mean_sqr_error( self.train_df.travel_time/60., self.train_df.LRegression_tt_predMean_direct)
+        self.root_mean_sqr_error( self.train_df.travel_time/60., self.train_df.RForest_tt_predMean_direct)
+        self.root_mean_sqr_error( self.train_df.travel_time/60., self.train_df.LGBM_tt_predMean_direct)
+        print("test")
+        self.root_mean_sqr_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logn)
+        self.root_mean_sqr_error(self.test_df.travel_time/60., self.test_df.hbr_tt_predMean_logdirect)
+        # self.root_mean_sqr_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean) 
+        self.root_mean_sqr_error(self.test_df.travel_time/60., self.test_df.ngboost_tt_predMean_direct)
+        self.root_mean_sqr_error( self.test_df.travel_time/60., self.test_df.LRegression_tt_predMean_direct)
+        self.root_mean_sqr_error( self.test_df.travel_time/60., self.test_df.RForest_tt_predMean_direct)
+        self.root_mean_sqr_error( self.test_df.travel_time/60., self.test_df.LGBM_tt_predMean_direct)
+  
+
+        
+  
+
+        # # print('**** for hbr model the errors on udtt are: ')
+        # # print("*** on train:")
+        # # self.mean_relative_error( self.train_df.unitDist_TT_hpm, self.train_df.hbr_udtt_predMean)
+        # # self.mean_absolute_error(self.train_df.unitDist_TT_hpm, self.train_df.hbr_udtt_predMean)
+        # # print("**** on test:")
+        # # self.mean_relative_error( self.test_df.unitDist_TT_hpm, self.test_df.hbr_udtt_predMean)
+        # # self.mean_absolute_error(self.test_df.unitDist_TT_hpm, self.test_df.hbr_udtt_predMean)
+
+
+        # # print('**** for ngboost model the errors on udtt are: ')
+        # # print("*** on train:")
+        # # self.mean_relative_error( self.train_df.unitDist_TT_hpm, self.train_df.ngboost_udtt_predMean)
+        # # self.mean_absolute_error(self.train_df.unitDist_TT_hpm, self.train_df.ngboost_udtt_predMean)
+        # # print("**** on test:")
+        # # self.mean_relative_error( self.test_df.unitDist_TT_hpm, self.test_df.ngboost_udtt_predMean)
+        # # self.mean_absolute_error(self.test_df.unitDist_TT_hpm, self.test_df.ngboost_udtt_predMean)
+
+    def plot_predictions_byRegions(self):
+        for pair in self.train_df['Orgn_Drgn'].unique():
+            d = self.train_df[self.train_df['Orgn_Drgn']==pair]
+            errors =  d['travel_time'][:]/60. - d['hbr_tt_predMean_direct'][:]
+            # sq_errors = (d['travel_time'][:]/60. - d['predictions1'][:]/60.)**2
+
+            d_tst = self.test_df[self.test_df['Orgn_Drgn']==pair]
+            errors_tst =  d_tst['travel_time'][:]/60. - d_tst['hbr_tt_predMean_direct'][:]
+
+            col = 4
+            fig, ax = plt.subplots(ncols = col, figsize=(7* col, 6))
+
+            ax[0].errorbar(x = d['travel_time'][:]/60, y = d['hbr_tt_predMean_direct'][:], yerr= d['hbr_tt_predStd_direct'][:], fmt='.', alpha=0.4, label=pair)
+            ax[0].plot([0,50],[0,50], color='k')
+            ax[0].set_xlabel('observed (min)', fontsize=22, fontdict={'weight': 'bold'})
+            ax[0].set_ylabel('predicted (min)', fontsize=22, fontdict={'weight': 'bold'})
+            # ax[0].set_yticklabels(ax[0].get_yticks().astype(int), size = 22, fontdict={'weight': 'bold'})
+            # ax[0].set_xticklabels(ax[0].get_xticks().astype(int), size = 22, fontdict={'weight': 'bold'})
+            ax[0].tick_params(axis='both', which='major', labelsize=22)
+            ax[0].legend(fontsize=22)
+
+            _ = ax[1].hist(errors, bins=50, density=True)
+            mean_rmse = np.sqrt(mean_squared_error(d['travel_time'][:]/60, d['hbr_tt_predMean_direct'][:]))
+            std_rmse = np.std(errors)
+            _=ax[1].set_title(" Train: RMSE = {:.2f} $\pm$ {:.2f} (min)".format(mean_rmse,std_rmse),fontsize=22, fontdict={'weight': 'bold'})
+            ax[1].set_xlabel('error(min)', fontsize=22, fontdict={'weight': 'bold'})
+            # ax[1].set_yticklabels(ax[1].get_yticks(), size = 22, fontdict={'weight': 'bold'})
+            # ax[1].set_xticklabels(np.round(ax[1].get_xticklabels(),1), size = 22, fontdict={'weight': 'bold'})
+            ax[1].tick_params(axis='both', which='major', labelsize=22)
+            ax[1].set_ylabel('')
+            ax[1].set_yticks([])
+
+            ax[2].errorbar(x = d_tst['travel_time'][:]/60., y = d_tst['hbr_tt_predMean_direct'][:], yerr= d_tst['hbr_tt_predStd_direct'][:], fmt='r.', alpha=0.4, label=pair)
+            ax[2].plot([0,50],[0,50], color='k')
+            ax[2].set_xlabel('observed (min)', fontsize=22, fontdict={'weight': 'bold'})
+            # ax[2].set_ylabel('predicted (min)', fontsize=22, fontdict={'weight': 'bold'})
+            # ax[2].set_yticklabels(ax[2].get_yticks().astype(int), size = 22, fontdict={'weight': 'bold'})
+            # ax[2].set_xticklabels(ax[2].get_xticks().astype(int), size = 22, fontdict={'weight': 'bold'})
+            ax[2].tick_params(axis='both', which='major', labelsize=22)
+            ax[2].legend(fontsize=22)
+
+            _ = ax[3].hist(errors_tst, bins=50, density=True, color='r')
+            # ax[3].set_yticklabels(ax[3].get_yticks(), size = 22, fontdict={'weight': 'bold'})
+            # ax[3].set_xticklabels(ax[3].get_xticks(), size = 22, fontdict={'weight': 'bold'})
+            ax[3].tick_params(axis='both', which='major', labelsize=22)
+            mean_rmse_tst = np.sqrt(mean_squared_error(d_tst['travel_time'][:]/60,d_tst['hbr_tt_predMean_direct'][:]))
+            std_rmse_tst = np.std(errors_tst)
+            _=ax[3].set_title(" Test: RMSE = {:.2f} $\pm$ {:.2f} (min)".format(mean_rmse_tst, std_rmse_tst), fontsize=22, fontdict={'weight': 'bold'} )
+            ax[3].set_xlabel('error(min)', fontsize=22, fontdict={'weight': 'bold'})
+            ax[3].set_ylabel('')
+            ax[3].set_yticks([])
+        plt.show()
---
+++ randomForest.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+import pickle
+from datetime import datetime
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+class RForest(object):
+    def __init__(self, data_type=None, test=False, pred=False, direct=True):
+        self.data_type = data_type
+        self.direct = direct
+        self.load_data()
+        
+        if test:
+            self.fit_model()
+        elif pred:
+            self.load_model()
+            self.test_model()
+            self.plot_predictions()
+            # self.predictions = {}
+            self.save_predictions()
+        else:
+            self.fit_model()
+            self.test_model()
+            self.plot_predictions()
+            self.save_predictions()
+        
+    
+    def load_data(self):
+        data_path = '/home/sevin/Desktop/projects/TravelTime_Prediction/HBRegression/TT_pred_wHBR/data/modeling_data/{}/'.format(self.data_type)
+        self.train_df = pd.read_csv(data_path + 'train_wpreds.txt')
+        # print(self.train_df['Orgn_Drgn'].unique())
+        # exit()
+        self.test_df = pd.read_csv(data_path + 'test_wpreds.txt')
+        with open(data_path + 'scale_parameters.pickle', 'rb') as handle:
+            scale_parameters = pickle.load(handle)
+        
+        self.variables_means = scale_parameters['means']
+        self.variables_stds = scale_parameters['stds']
+        if self.direct:
+            self.X_train = self.train_df[['shortPath_mile', 'adverse_weather','highway','path_avg_bwness','path_avg_degree','cos_theta','sin_theta']].values
+            self.X_test = self.test_df[['shortPath_mile', 'adverse_weather','highway','path_avg_bwness','path_avg_degree','cos_theta','sin_theta']].values
+            # self.Y_train = self.train_df['unitDist_TT_hpm'].values
+            # self.Y_test = self.test_df['unitDist_TT_hpm'].values
+            self.Y_train = self.train_df['travel_time'].values
+            self.Y_test = self.test_df['travel_time'].values
+        else:
+            self.X_train = self.train_df[['adverse_weather','highway','path_avg_bwness','path_avg_degree','cos_theta','sin_theta']].values
+            self.X_test = self.test_df[['adverse_weather','highway','path_avg_bwness','path_avg_degree','cos_theta','sin_theta']].values
+            self.Y_train = self.train_df['unitDist_TT_hpm'].values
+            self.Y_test = self.test_df['unitDist_TT_hpm'].values
+    
+    def fit_model(self):
+        self.RF= RandomForestRegressor(n_estimators = 20, random_state = 0)
+        self.RF.fit(self.X_train, self.Y_train)
+        save_path = '/home/sevin/Desktop/projects/TravelTime_Prediction/HBRegression/TT_pred_wHBR/trained_models/{}/RandomForest/'.format(self.data_type)
+        if self.direct:
+            with open(save_path + 'model1_MN_RForest_direct.pickle', 'wb') as buff:
+                pickle.dump(self.RF, buff)
+        else:
+            with open(save_path + 'model1_MN_RForest.pickle', 'wb') as buff:
+                pickle.dump(self.RF, buff)
+    
+    def load_model(self):
+        save_path = '/home/sevin/Desktop/projects/TravelTime_Prediction/HBRegression/TT_pred_wHBR/trained_models/{}/RandomForest/'.format(self.data_type)
+        if self.direct:
+            with open(save_path + 'model1_MN_RForest_direct.pickle', 'rb') as buff:
+                self.RF = pickle.load(buff)
+        else:
+            with open(save_path + 'model1_MN_RForest.pickle', 'rb') as buff:
+                self.RF = pickle.load(buff)
+    
+    def mean_relative_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MRE = np.mean(np.abs((y_true - y_pred) / y_true)) 
+        # print('mean relative error is {:0.4f}'.format(MRE) )
+        return MRE
+    def mean_absolute_error(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        MAE = np.mean(np.abs(y_true - y_pred)) #* 60
+        # print('mean absolute error is {:0.2f} '.format( MAE) )
+        return MAE
+    
+    def test_model(self):
+        self.Y_preds_tst = self.RF.predict(self.X_test)
+        # self.Y_dists_tst = self.LR.pred_dist(self.X_test) #.params = {'loc':, 'scale':}
+        # self.std_preds_tst = self.Y_dists_tst.params['scale']
+        # print(self.std_preds_tst)
+        # print(self.std_preds_tst.shape)
+        # exit()
+        self.test_MSE = mean_squared_error(self.Y_preds_tst, self.Y_test)
+        print('Test MSE', np.sqrt(self.test_MSE))
+        print("Test mean relative error {:0.4f}".format(self.mean_relative_error(self.Y_test, self.Y_preds_tst)))
+        print("Test mean absolute error {:0.2f}".format(self.mean_absolute_error(self.Y_test, self.Y_preds_tst)))
+        
+        # test Negative Log Likelihood
+        # self.test_NLL = -self.Y_dists_tst.logpdf(self.Y_test).mean()
+        # print('Test NLL', self.test_NLL)
+
+        self.Y_preds_tr = self.RF.predict(self.X_train)
+        # self.Y_dists_tr = self.ngb.pred_dist(self.X_train)
+        # self.std_preds_tr = self.Y_dists_tr.params['scale']
+
+        # test Mean Squared Error
+        self.train_MSE = mean_squared_error(self.Y_preds_tr, self.Y_train)
+        print('Train MSE', np.sqrt(self.train_MSE))
+        print("Train mean relative error {:0.4f}".format(self.mean_relative_error(self.Y_train, self.Y_preds_tr)))
+        print("Train mean absolute error {:0.2f}".format(self.mean_absolute_error(self.Y_train, self.Y_preds_tr)))
+        # # test Negative Log Likelihood
+        # self.train_NLL = -self.Y_dists_tr.logpdf(self.Y_train).mean()
+        # print('Train NLL', self.train_NLL)
+
+    def plot_predictions(self):
+        nplot = 10000
+        fig, ax = plt.subplots(ncols = 2, figsize=(10,5))
+        # ax[0].plot(self.Y_train[:nplot], self.Y_preds_tr[:nplot],'.' )
+        ax[0].plot(self.Y_train[:nplot], self.Y_preds_tr[:nplot], 'r.', alpha=0.4)
+        ax[0].plot([0,1000],[0,1000])
+        ax[0].set_xlabel('True')
+        ax[0].set_ylabel('Pred')
+        # ax[1].plot(self.Y_test[:nplot], self.Y_preds_tst[:nplot],'.')
+        ax[1].plot(self.Y_test[:nplot], self.Y_preds_tst[:nplot], 'r.', alpha=0.4)
+        ax[1].plot([0,1000],[0,1000])
+        ax[1].set_xlabel('True')
+        ax[1].set_ylabel('Pred')
+        plt.show()
+    
+    def save_predictions(self):
+
+        if self.direct:
+            self.train_df['RForest_tt_predMean_direct'] = self.Y_preds_tr/60.
+            # self.train_df['LRegression_tt_predStd_direct'] = self.std_preds_tr/60.
+
+            self.test_df['RForest_tt_predMean_direct'] = self.Y_preds_tst/60.
+            # self.test_df['LRegression_tt_predStd_direct'] = self.std_preds_tst/60.
+
+        else:
+            pass
+            # self.train_df['LRegression_udtt_predMean'] = self.Y_preds_tr
+            # self.train_df['LRegression_udtt_predStd'] = self.std_preds_tr
+
+            # self.test_df['LRegression_udtt_predMean'] = self.Y_preds_tst
+            # self.test_df['LRegression_udtt_predStd'] = self.std_preds_tst
+
+            # miles = self.train_df['shortPath_mile'].values
+            # self.train_df['LRegression_tt_predMean'] = self.train_df.ngboost_udtt_predMean[:] * miles * 60
+            # self.train_df['LRegression_tt_predStd'] = self.train_df.ngboost_udtt_predStd * np.sqrt( miles) * 60
+
+            # miles = self.test_df['shortPath_mile'].values
+            # self.test_df['ngboost_tt_predMean'] = self.test_df.ngboost_udtt_predMean * miles * 60
+            # self.test_df['ngboost_tt_predStd'] = self.test_df.ngboost_udtt_predStd * np.sqrt( miles) * 60
+            
+
+        data_path = '/home/sevin/Desktop/projects/TravelTime_Prediction/HBRegression/TT_pred_wHBR/data/modeling_data/{}/'.format(self.data_type)
+        self.train_df.to_csv(data_path + 'train_wpreds.txt')
+        self.test_df.to_csv(data_path + 'test_wpreds.txt')
